3e02e13f0c0c2c61401a3bebba892258413696a2
ZIMUF27 <poo2461p@gmail.com>
2026-03-05 13:57:28 +0700

    Update template service to use backend API and clean up logic

--- frontend/src/app/services/template.service.ts
+++ frontend/src/app/services/template.service.ts
@@ -1,49 +1,24 @@
 import { environment } from "../../environments/environment";
 import { Injectable } from '@angular/core';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TemplateService {
-    private supabase: SupabaseClient;
+    private apiUrl = `${environment.apiUrl}/templates`;
 
-    constructor() {
-        this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<any>(this.apiUrl);
     }
 
-    getAll(): Observable<any> {
-        return from(this.supabase.from('Template').select('*')).pipe(
-            map(res => {
-                if (res.error) throw res.error;
-                return { success: true, data: res.data };
-            })
-        );
+    getById(id: string) {
+        return this.http.get<any>(`${this.apiUrl}/${id}`);
     }
 
-    getById(id: string): Observable<any> {
-        return from(
-            this.supabase
-                .from('Template')
-                .select('*')
-                .eq('id', id)
-                .single()
-        ).pipe(
-            map(res => {
-                if (res.error) throw res.error;
-                return { success: true, data: res.data };
-            })
-        );
-    }
-
-    create(data: any): Observable<any> {
-        return from(this.supabase.from('Template').insert(data).select().single()).pipe(
-            map(res => {
-                if (res.error) throw res.error;
-                return { success: true, data: res.data };
-            })
-        );
+    create(data: any) {
+        return this.http.post<any>(this.apiUrl, data);
     }
 }
