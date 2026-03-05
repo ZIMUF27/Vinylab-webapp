0dbc6b2d1b4a613fda223c2f79f88a4ce33b10dd
ZIMUF27 <poo2461p@gmail.com>
2026-03-05 13:41:33 +0700

    fix: refactor TemplateService to use Supabase Data API directly for better reliability

--- frontend/src/app/services/template.service.ts
+++ frontend/src/app/services/template.service.ts
@@ -1,24 +1,49 @@
 import { environment } from "../../environments/environment";
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TemplateService {
-    private apiUrl = `${environment.apiUrl}/templates`;
+    private supabase: SupabaseClient;
 
-    constructor(private http: HttpClient) { }
-
-    getAll() {
-        return this.http.get<any>(this.apiUrl);
+    constructor() {
+        this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
     }
 
-    getById(id: string) {
-        return this.http.get<any>(`${this.apiUrl}/${id}`);
+    getAll(): Observable<any> {
+        return from(this.supabase.from('Template').select('*')).pipe(
+            map(res => {
+                if (res.error) throw res.error;
+                return { success: true, data: res.data };
+            })
+        );
     }
 
-    create(data: any) {
-        return this.http.post<any>(this.apiUrl, data);
+    getById(id: string): Observable<any> {
+        return from(
+            this.supabase
+                .from('Template')
+                .select('*')
+                .eq('id', id)
+                .single()
+        ).pipe(
+            map(res => {
+                if (res.error) throw res.error;
+                return { success: true, data: res.data };
+            })
+        );
+    }
+
+    create(data: any): Observable<any> {
+        return from(this.supabase.from('Template').insert(data).select().single()).pipe(
+            map(res => {
+                if (res.error) throw res.error;
+                return { success: true, data: res.data };
+            })
+        );
     }
 }
